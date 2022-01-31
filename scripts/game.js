574d2c67e4e9a22ba1bf163c7d3587337b3b81ad
Carwi Barrios <carwelsbarrios@gmail.com>
2022-01-31 16:18:53 +0100

    update

--- scripts/game.js
+++ scripts/game.js
@@ -1,15 +1,28 @@
 //First thing we get our canvas
-const canvas = document.getElementById("canvas")
-const ctx = canvas.getContext("2d")
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-function animation() {
-    requestAnimationFrame(animation)
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //canvas
-    drawBackgroundLine()
+let score = 0;
 
-    player.draw()
+//3***drawScore in the canvas
+function drawScore() {
+  ctx.font = "50px sans-serif";
+  ctx.fillStyle = "black";
+  let scoreString = score.toString(); //<- First convert score into a string
+  let xOffSet = (scoreString.length - 1) * 20; // To position score in the middle of the screen
+  ctx.fillText(scoreString, 280 - xOffSet, 100); // x:280 - y:100
 }
 
-animation()
 
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //Canvas
+  drawBackgroundLine();
+  //Score
+  drawScore();
+
+  player.draw();
+}
+
+animate();
--- scripts/player.js
+++ scripts/player.js
@@ -1,15 +1,51 @@
+//2*** second thing is to create the player to appear in the canvas
 class Player {
-    constructor(x, y, size, color) {
-        this.x = x;
-        this.y = y;
-        this.size = size;
-        this.color = color;
-    }
+  constructor(x, y, size, color) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color;
+    //Jump configuration
+    this.jumpHeight = 12; //<- frames to go up while jumping
+    this.shouldJump = false;
+    this.jumpCounter = 0;
+  }
 
-    draw() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.size, this.size)
+  jump() {
+    if (this.shouldJump) {
+      this.jumpCounter++;
+      if (this.jumpCounter < 15) {
+        //Go up jump
+        this.y -= this.jumpHeight;
+      } else if (this.jumpCounter > 14 && this.jumpCounter < 19) {
+        this.y += 0;
+      } else if (this.jumpCounter < 33) {
+        //Come back down from jump
+        this.y += this.jumpHeight;
+      }
+      //End the jumping
+      if (this.jumpCounter >= 32) {
+        this.shouldJump = false;
+      }
     }
+  }
+
+  // The draw method is so that it will get the player class to render to the canvas
+  draw() {
+    this.jump(); // jump() needs to be called before the player is drawn to the canvas, that way the jump can be seen
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+  }
 }
 
-let player = new Player(150,350,50,"black")
+let player = new Player(150, 350, 50, "black");
+
+addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
+    if (!player.shouldJump) {
+      // This will prevent the user from double jumping while still in mid air
+      player.jumpCounter = 0;
+      player.shouldJump = true;
+    }
+  }
+});
